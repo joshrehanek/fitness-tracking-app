e1f40c632824f02cfbfd3e166a4b0e73db90ea95
Josh Rehanek <joshrehanek@gmail.com>
2021-03-17 15:23:46 -0700

    deletes schema var

--- models/Workout.js
+++ models/Workout.js
@@ -1,9 +1,7 @@
 // define schema
 const mongoose = require("mongoose");
 
-const Schema = mongoose.Schema;
-
-const WorkoutSchema = new Schema({
+const WorkoutSchema = new mongoose.Schema({
     day: {
         type: Date,
         default: () => new Date()
